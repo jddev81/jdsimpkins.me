c36f03f41a453201cae62211a072a9bef405dac4
jddev81 <47901060+jddev81@users.noreply.github.com>
2019-03-26 11:42:33 -0400

    Mobile Menu Fix

--- inc/nav.php
+++ inc/nav.php
@@ -4,19 +4,19 @@
 
 		<ul>
 
-			<li class="pi"><a href="#home" class="active">Home</a></li>
+			<li class="pi" onclick="removeMenu();"><a href="#home" class="active">Home</a></li>
 
-			<li class="pi"><a href="#about">About</a></li>
+			<li class="pi" onclick="removeMenu();"><a href="#about">About</a></li>
 
-			<li class="pi"><a href="#skills">Skills</a></li>
+			<li class="pi" onclick="removeMenu();"><a href="#skills">Skills</a></li>
 
-			<li class="pi"><a href="#xp">Experience</a></li>
+			<li class="pi" onclick="removeMenu();"><a href="#xp">Experience</a></li>
 
-			<li class="pi"><a href="#portfolio">Portfolio</a></li>
+			<li class="pi" onclick="removeMenu();"><a href="#portfolio">Portfolio</a></li>
 
 			<!-- <li><a href="#">Blog</a></li> -->
 
-			<li class="pi"><a href="#contact">Contact</a></li>
+			<li class="pi" onclick="removeMenu();"><a href="#contact">Contact</a></li>
 
 			<li class="mobile-nav-icon"><a href="javascript:void(0);" onclick="toggleMenu()"><i class="fa fa-bars"></i></a></li>
 
@@ -28,5 +28,8 @@
 <script>
 	function toggleMenu() {
 		$('.pi').toggleClass('shown');
+	}
+	function removeMenu() {
+		$('.pi').removeClass('shown');
 	}
 </script>
